65768c6f93d1eb1ade9ee0b22deaab8b5a0947d1
NickDomanti <domanti.nic@gmail.com>
2026-04-08 17:13:21 +0200

    added route transition

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,12 +1,30 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
 <template>
   <UApp>
     <UContainer class="h-svh flex flex-col justify-center items-center gap-4">
-      <div class="flex flex-col items-center">
-        <NuxtImg src="/img/d20.webp" sizes="md:400px" preload />
-        <NuxtImg src="/img/wh.webp" sizes="md:400px" preload />
-      </div>
+      <Transition name="images" mode="out-in">
+        <div :key="route.path" class="flex flex-col items-center">
+          <NuxtImg src="/img/d20.webp" sizes="md:400px" preload />
+          <NuxtImg src="/img/wh.webp" sizes="md:400px" preload />
+        </div>
+      </Transition>
 
       <slot />
     </UContainer>
   </UApp>
 </template>
+
+<style scoped>
+.images-enter-active,
+.images-leave-active {
+  transition: transform 0.3s ease;
+}
+
+.images-enter-from,
+.images-leave-to {
+  transform: scale(0.95);
+}
+</style>
