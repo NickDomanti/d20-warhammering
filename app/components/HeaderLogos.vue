caadfbdb6a3f6555de46ba38a19a97afcded4492
NickDomanti <domanti.nic@gmail.com>
2026-05-04 13:44:54 +0200

    header logos are now inside header tag

--- app/components/HeaderLogos.vue
+++ app/components/HeaderLogos.vue
@@ -5,12 +5,14 @@ defineProps<{
 </script>
 
 <template>
-  <NuxtLink
-    to="/"
-    class="flex flex-col justify-center items-center"
-    :class="small ? 'w-30 md:w-50' : 'w-70'"
-  >
-    <NuxtImg src="/img/d20.webp" preload />
-    <NuxtImg src="/img/wh.webp" preload />
-  </NuxtLink>
+  <header>
+    <NuxtLink
+      to="/"
+      class="flex flex-col justify-center items-center"
+      :class="small ? 'w-30 md:w-50' : 'w-70'"
+    >
+      <NuxtImg src="/img/d20.webp" preload />
+      <NuxtImg src="/img/wh.webp" preload />
+    </NuxtLink>
+  </header>
 </template>
